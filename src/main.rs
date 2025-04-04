e07604d8bfc9ac5b9bb8cd83a863807278850f14
= <=>
2025-04-04 22:40:49 +0100

    feat: add new branch

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+struct Employee {
+    first_name: String,
+    last_name: String,
+    role: String,
+    department: String,
+}
+
+fn main() {
+    let _alpha = Employee {
+        first_name: "Peter".to_string(),
+        last_name: "Smith".to_string(),
+        role: "Teacher".to_string(),
+        department: "English".to_string(),
+    };
+
+    print_employee(_alpha);
+
+    print_employee(create_employee("John".to_string(), "Smith".to_string(), "Chemistry".to_string(), "Science".to_string()));
+}
+
+fn create_employee(first_name: String, last_name: String, role: String, department: String) -> Employee {
+    Employee {
+        first_name,
+        last_name,
+        role,
+        department
+    }
+}
+
+fn print_employee(employee: Employee) {
+    println!(
+        "{} {}. Role: {}. Department: {}",
+        employee.first_name, employee.last_name, employee.role, employee.department
+    );
+}
